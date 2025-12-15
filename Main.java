96b7cb45bc18f2b7a2d9d5478d9dd1578fbd0cba
Het <hetkothari610@gmail.com>
2025-12-16 04:31:37 +0530

    commit employee and main java file changes

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!");
+
+        System.out.println("This is a sample Java program.");    
+
+        Employee emp = new Employee("Alice", "Engineering");
+        System.out.println("Employee Name: " + emp.getName());
+        System.out.println("Employee Department: " + emp.getDepartment());
+    }
+    
+}
